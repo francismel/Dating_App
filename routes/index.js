db89878328972873d70108c63b2c0364c6853266
Francis Mel <fmeldefo@usc.edu>
2020-03-04 11:15:12 -0800

    redirects to correct place

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
-  res.redirect('/daters/index');
+  res.redirect('/daters');
 });
 
 // Google OAuth login route
